e460ff154babcb6e6df6631c920388020d3813be
pwrmin <pwrmin06@gmail.com>
2025-10-31 15:03:31 +0900

    15:03 park React file , contacts , org

--- src/pages/Contacts/OrganizationView.jsx
+++ src/pages/Contacts/OrganizationView.jsx
@@ -7,18 +7,18 @@ const OrganizationView = ({ member, onClose }) => {
     return (
 
 
-        <div className={styles.container}>
+        <div className={styles.container} >
 
             <div className={styles.mainHeader} style={{marginTop:"20px"}}>
                 <h2>{member.name} / {member.dept_code} / {member.rank_code}</h2>
             </div>
             <hr></hr>
-            <div className={styles.mainBody} style={{ fontSize: "20px" , marginTop:"30px" }}>
+            <div className={styles.mainBody} style={{ fontSize: "20px" , marginTop:"30px" , border:"none" }}>
                 <p>아이디: {member.id}</p>
-                <p>회사이메일: {member.officeEmail}</p>
-                <p>개인이메일: {member.personalEmail}</p>
-                <p>회사번호: {member.officePhone}</p>
-                <p>개인번호: {member.mobilePhone}</p>
+                <p>회사 이메일: {member.officeEmail}</p>
+                <p>개인 이메일: {member.personalEmail}</p>
+                <p>회사 번호: {member.officePhone}</p>
+                <p>개인 번호: {member.mobilePhone}</p>
                 <p>주소: {member.address_line1} {member.address_line2}</p>
 
                 <button onClick={onClose} style={{ float: "right" }}>닫기</button>
