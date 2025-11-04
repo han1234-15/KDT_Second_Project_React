556d71f6693c1eb47b1fcb059db10124b0c06445
Carrythefuture <146246027+Carrythefuture@users.noreply.github.com>
2025-11-04 15:23:59 +0900

    options 연구&개발 -> 연구개발, 사업관리팀 -> 사업관리 수정

--- src/config/options.js
+++ src/config/options.js
@@ -1,6 +1,6 @@
 export const departmentOptions = [
   { value: '연구개발', label: '연구개발' },
-  { value: '사업관리팀', label: '사업관리팀' },
+  { value: '사업관리', label: '사업관리' },
   { value: 'AI센터', label: 'AI센터' },
   { value: '인사관', label: '인사관' },
   { value: '재무/회계', label: '재무/회계' },
